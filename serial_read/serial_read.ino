09b05d7552457e93b1d198968156e0d6943d94de
Dowling_Wong <80257366+Dowling7@users.noreply.github.com>
2022-11-18 11:13:51 -0500

    correction

--- serial_read/serial_read.ino
+++ serial_read/serial_read.ino
@@ -1,7 +1,7 @@
 int forward = 4;    
-int backward = 4;    
-int left = 4;    
-int right = 4;    
+int backward = 5;    
+int left = 6;    
+int right = 7;    
 
 int val1 = 0;      // variable to store the read value
 int val2 = 0;
@@ -26,6 +26,6 @@ void loop() {
   Serial.print(val1);
   Serial.print(val2);
   Serial.print(val3);
-  Serial.print(val4);
+  Serial.println(val4);
 
 }
